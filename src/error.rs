57c0660e416144e1588cc9f3b4ab3c6935ef4287
Luis Schwab <luisschwab@protonmail.com>
2025-11-06 13:24:01 -0300

    feat(faucet): add LBTC faucet method

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,9 @@ use thiserror::Error;
 /// Unified error variants.
 #[derive(Debug, Error)]
 pub(crate) enum Error {
+    #[error("Bitreq error: {0}")]
+    Bitreq(#[from] bitreq::Error),
+
     #[error("Esplora error: {0}")]
     Esplora(#[from] lwk_wollet::Error),
 }
---
+++ src/faucet.rs
@@ -0,0 +1,36 @@
+use bitreq::Response;
+use elements::Address;
+use tracing::info;
+
+use crate::error::Error;
+
+/// L-BTC TestnetV1 faucet URL.
+pub(crate) const FAUCET_URL: &str =
+    "https://liquidtestnet.com/faucet?address=<PLACEHOLDER>&action=lbtc";
+
+/// Request L-BTC TestnetV1 coins to an address.
+pub(crate) async fn get_coins(address: &Address) -> Result<Response, Error> {
+    let url: String = FAUCET_URL.replace("<PLACEHOLDER>", &address.to_string());
+
+    info!("Requesting L-BTC coin from faucet: {}", url);
+    let response: Response = bitreq::get(&url).send_async().await?;
+    info!("Received response code {}", response.status_code);
+
+    Ok(response)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get_coins() {
+        tracing_subscriber::fmt().init();
+
+        let address: Address = "ex1qxdrsg0z86ca6q848jyczql8c5zkuax9n90kk6t"
+            .parse::<Address>()
+            .unwrap();
+
+        let _ = get_coins(&address).await.unwrap();
+    }
+}
